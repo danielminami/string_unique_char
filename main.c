0145bce5acd15bdd379d937502db06229d4091fb
Daniel <danielminami@hotmail.com>
2020-07-07 11:29:54 -0400

    First Commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+Daniel Minami <minamid@sheridancollege.ca>
+----------------------------------------------------------------------
+ */
+
+/* 
+ * File:   main.c
+ * Author: Daniel Minami <minamid@sheridancollege.ca>
+ *
+ * Created on July 7, 2020, 11:29 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
